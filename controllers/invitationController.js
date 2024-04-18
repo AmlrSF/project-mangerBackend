e946a1ce6af08ab6d8485dd98cbad0b99d538523
softyAmir <theyabcpssouaf2@gmail.com>
2024-04-18 08:48:47 -1100

    .

--- controllers/invitationController.js
+++ controllers/invitationController.js
@@ -2,7 +2,7 @@ const Invitation = require('../schema/invitation');
 const Project = require('../schema/projects');
 
 const Customer = require("../schema/customers");
-
+d
 // Get all invitations
 async function getAllInvitations(req, res) {
   try {
